e86639efb62be6742908f19aea68a988d842a2e3
Michael-merlot <124175319+Michael-merlot@users.noreply.github.com>
2024-05-30 14:52:12 +0300

    Update and fixing locations

--- Assets/Scripts/EnemyPatrol.cs
+++ Assets/Scripts/EnemyPatrol.cs
@@ -1,15 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Transactions;
 using UnityEngine;
 
 public class EnemyPatrol : MonoBehaviour
 {
     public float speed;
-
     public int positionOfPatrol;
     public Transform point;
-    bool moveingRight;
+    bool movingRight = true; // Ќачальное направление движени€ вправо
 
     Transform Player;
     public float stoppingDistance;
@@ -17,7 +15,6 @@ public class EnemyPatrol : MonoBehaviour
     bool chill = false;
     bool angry = false;
     bool goBack = false;
-
 
     void Start()
     {
@@ -41,8 +38,6 @@ public class EnemyPatrol : MonoBehaviour
             goBack = true;
         }
 
-
-
         if (chill == true)
         {
             Chill();
@@ -55,22 +50,22 @@ public class EnemyPatrol : MonoBehaviour
         {
             GoBack();
         }
-
     }
-
 
     void Chill()
     {
         if (transform.position.x > point.position.x + positionOfPatrol)
         {
-            moveingRight = false;
+            movingRight = false;
+            Flip();
         }
         else if (transform.position.x < point.position.x - positionOfPatrol)
         {
-            moveingRight = true;
+            movingRight = true;
+            Flip();
         }
 
-        if (moveingRight)
+        if (movingRight)
         {
             transform.position = new Vector2(transform.position.x + speed * Time.deltaTime, transform.position.y);
         }
@@ -78,16 +73,34 @@ public class EnemyPatrol : MonoBehaviour
         {
             transform.position = new Vector2(transform.position.x - speed * Time.deltaTime, transform.position.y);
         }
-
     }
 
     void Angry()
     {
+        if ((Player.position.x > transform.position.x && !movingRight) || (Player.position.x < transform.position.x && movingRight))
+        {
+            movingRight = !movingRight;
+            Flip();
+        }
+
         transform.position = Vector2.MoveTowards(transform.position, Player.position, speed * Time.deltaTime);
     }
 
     void GoBack()
     {
+        if ((point.position.x > transform.position.x && !movingRight) || (point.position.x < transform.position.x && movingRight))
+        {
+            movingRight = !movingRight;
+            Flip();
+        }
+
         transform.position = Vector2.MoveTowards(transform.position, point.position, speed * Time.deltaTime);
+    }
+
+    void Flip()
+    {
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 }
